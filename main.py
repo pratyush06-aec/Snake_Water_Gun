6c7246327ef0498d010259a660c4d9436e0bfa84
pratyush06-aec <pratyush00710@gmail.com>
2025-09-19 08:08:46 +0530

    main.py

--- main.py
+++ main.py
@@ -2,49 +2,47 @@ import random
 
 computer_choice= random.choice([0, -1, 1])
 
-try:
-    a= str(input("Enter your name: "))
-    print(a)
+a= str(input("Enter your name: "))
 
-except ValueError as v:
-    print(f"Hii {a}, we don't accept any numeric value here!!!!")
+if a.isnumeric():
+    raise ValueError("Your name can't be in numeric format or have digits in it. Try again next time!!!")
+else:
+    print(f"Welcome to this game {a}!!!")
 
-print(f"Welcome to this game {a}!!!")
+    try:
+        you_dict= {"Snake":0, "Gun":1, "Water":-1}
+        reverse_you_dict= {0:"Snake", 1:"Gun", -1:"Water"}
 
-try:
-    you_dict= {"Snake":0, "Gun":1, "Water":-1}
-    reverse_you_dict= {0:"Snake", 1:"Gun", -1:"Water"}
+        you= input("Enter your choice: ")
+        your_choice= you_dict[you]
 
-    you= input("Enter your choice: ")
-    your_choice= you_dict[you]
-
-    print(f"You choose {reverse_you_dict[your_choice]}\nComputer choose {reverse_you_dict[computer_choice]}")
+        print(f"You choose {reverse_you_dict[your_choice]}\nComputer choose {reverse_you_dict[computer_choice]}")
 
 
-    if(computer_choice==your_choice):
-        print("It's a draw!!!")
-
-    else:
-        if(computer_choice==1 and your_choice==0):
-            print("Computer win!!!!")
-
-        elif(computer_choice==1 and your_choice==-1):
-            print("You win!!!")
-
-        elif(computer_choice==0 and your_choice==1):
-            print("You win!!!")
-
-        elif(computer_choice==0 and your_choice==-1):
-            print("Computer win!!!")
-
-        elif(computer_choice==-1 and your_choice== 0):
-            print("Computer win!!!")
-
-        elif(computer_choice==-1 and your_choice==1):
-            print("You win!!!")
+        if(computer_choice==your_choice):
+            print("It's a draw!!!")
 
         else:
-            print("Something went wrong!!!")
+            if(computer_choice==1 and your_choice==0):
+                print("Computer won!!!!")
 
-except KeyError as k:
-    print(f"Hey {a}, you have raised a key-error. Better luck next time!!!!")
+            elif(computer_choice==1 and your_choice==-1):
+                print("You won!!!")
+
+            elif(computer_choice==0 and your_choice==1):
+                print("You won!!!")
+
+            elif(computer_choice==0 and your_choice==-1):
+                print("Computer won!!!")
+
+            elif(computer_choice==-1 and your_choice== 0):
+                print("You won!!!")
+
+            elif(computer_choice==-1 and your_choice==1):
+                print("Computer won!!!")
+
+            else:
+                print("Something went wrong!!!")
+
+    except KeyError as k:
+        print(f"Hey {a}, you have raised a key-error. Better luck next time!!!!")
